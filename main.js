c9f4085abf0a2dc5e4098a6d924a99f2e3fb500a
Ochanissi <mirelalex2094@gmail.com>
2019-02-12 12:59:57 +0200

    Finishing the base game

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', init);
+
+// Available Levels
+const levels = {
+    easy: 5,
+    medium: 3,
+    hard: 1
+}
+
+// To change level
+const currentLevel = levels.easy;
+
 // Globals
-let time = 5;
+let time = currentLevel;
 let score = 0;
 let isPlaying;
+
 
 // DOM Elements
 const wordInput = document.querySelector('#word-input');
@@ -29,13 +41,44 @@ const words = [
 
 // Initialize Game
 function init() {
+ // Show number of seconds in UI
+ seconds.innerHTML = currentLevel;
  // Load word from array
  showWord(words);
- 
+ // Start matching on word input
+ wordInput.addEventListener('input', startMatch);
  //Call countdown every second
  setInterval(countdown, 1000);
  // Check game status
  setInterval(checkStatus, 50);
+}
+
+// Start Match
+function startMatch() {
+    if(matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+    // If score is -1, display 0/
+    if(score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+// Match currentWord to wardInput
+function matchWords() {
+    if(wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 // Pick & Show Random Word
@@ -65,5 +108,6 @@ function countdown() {
 function checkStatus() {
     if(!isPlaying && time === 0) {
         message.innerHTML = 'Game Over!';
+        score = -1;
     }
 }
